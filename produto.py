e6d375ddd60fb395227cb14f379ea39a26efe2d5
GabrielPedrosa <gabriel.pedrosaf@gmail.com>
2020-04-05 00:16:05 -0300

    Pronto para Testes Finais

--- produto.py
+++ produto.py
@@ -1,30 +1,60 @@
 #coding:utf-8
 class Produto(object):
     def __init__(self):
-        print("Inicilizando...\n")
+        print("Inicilizando...")
         
     def criar(self, size):
-        print("Criando Lista...\n")
+        print("Criando Lista...")
         self.size = size
         self.list = [None]*size
-        print(("Criado\n"))
+        self.increase = 0
+        print(("Criado"))
 
     def inserir(self, position, produto):
-        print("Inserindo Produto...\n")
-        self.list[position] = produto
-        print("Inserido\n")
-    
+        if(self.size > self.increase):
+            print("Inserindo Produto...")
+            allow = True
+            other = False
+            if(self.list[position] != None ):
+                sure = int(input("Já existe um produto nessa posição.\nDeseja Sobreescrever: 1 - Sim, 2 - Não\n"))
+                if(sure == 1):
+                    allow = True
+                else:
+                    sure2 = int(input("Deseja inserir na proxima posição: 1 - Sim, 2 - Não\n"))
+                    if(sure2 == 1):
+                        cont = position
+                        for i in self.list:
+                            if(cont >= self.size):
+                                cont = 0
+                            if(i == None):
+                                next = cont
+                                break
+                            cont += 1
+                        self.inserir(next, produto)
+                        allow = True
+                        other = True
+                    else:
+                        allow = False
+            if allow and not(other):
+                self.list[position] = produto
+                print("Inserido")
+                self.increase += 1
+            elif not(allow) and not(other):
+                print("Não inserido")
+        else:
+            print("Lista Cheia!")
+
     def remover(self, position):
-        print("Removendo Produto...\n")
+        print("Removendo Produto...")
         self.list[position] = None
-        print("Removido\n")
+        print("Removido")
     
     def retornar(self, produto):
-        print("Retornar Nome do Produto...\n")
+        print("Retornar Nome do Produto...")
         cont = 0
         for i in self.list:
             if(i == produto):
-                print(cont, i)
+                print("Posição: "+str(cont)+" Produto: "+str(i))
             cont += 1
     
     def buscar(self, produto):
@@ -32,11 +62,11 @@ class Produto(object):
         cont = 0
         for i in self.list:
             if(i == produto):
-                print("Posição:"+str(cont))
+                print("Posição: "+str(cont))
             cont += 1
     
     def imprimir(self):
-        print("Imprimindo valor...\n")
+        print("Imprimindo valor...")
         cont = 0
         for i in self.list:
             if (i != None):
