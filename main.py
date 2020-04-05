f071a118497993788b5b79ac047266e5a0d375da
henrique <hherrmann.sul@gmail.com>
2020-04-04 22:13:50 -0300

    menu

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 #coding:utf-8
-class Main(object):
+class main(object):
     def __init__(self):
-        print("Escolha uma opção!\n 1 - Criar um produto\n 2 - Inserir um produto\n 3 - Remover um produto\n 4 - Retornar um produto\n 5 - Buscar um produto\n 6 - Imprimir a Lista")
+        continuar = True
+        while(continuar):
+            menu = int(input("Escolha uma opção!\n 1 - Criar um produto\n 2 - Inserir um produto\n 3 - Remover um produto\n 4 - Retornar um produto\n 5 - Buscar um produto\n 6 - Imprimir a Lista\n 7 - Sair\n"))
+            if(menu == 1):
+                print("1")
+            elif(menu == 2):
+                print("2")
+            elif(menu == 3):
+                print("3") 
+            elif(menu == 4):
+                print("4") 
+            elif(menu == 5):
+                print("5") 
+            elif(menu == 6):
+                print("6") 
+            elif(menu == 7):
+                continuar = False 
 
-Main()
+main()
